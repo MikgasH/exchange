5c55b8adfcafe8893ad5e98cce4f11540f94b4b5
Mikita Hashkin <m.hashkin@godeltech.com>
2025-09-02 19:30:45 +0300

    JM2MH-1: Initialize Spring Boot project

---
+++ src/main/java/com/example/cerpshashkin/CerpsHashkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.cerpshashkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerpsHashkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerpsHashkinApplication.class, args);
+	}
+
+}
